5e83d55c881fa3475e2cf79536412a32556d3786
Arthur Geel <ageel@enrise.com>
2023-12-02 12:07:58 +0100

    Add better placeholder popup page

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -2,8 +2,9 @@ import "@assets/styles/tailwind.css";
 
 export default function Popup(): JSX.Element {
   return (
-    <div className="max-h-screen flex flex-col max-w-[300px] w-full mx-auto">
-      <p>Hello world</p>
+    <div className="max-h-screen min-h-screen bg-sky-800 flex flex-col items-center justify-center max-w-[300px] w-full mx-auto gap-4">
+      <img src="/icon-128.png" />
+      <p className="font-bold text-base text-white">What&apos;s UpTab?</p>
     </div>
   );
 }
