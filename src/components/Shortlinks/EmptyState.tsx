881dc40f615c3758e282da5b85dc3c9c04b3dd40
Arthur Geel <ageel@enrise.com>
2023-12-02 17:42:47 +0100

    Reduce clutter on Empty Shortlinks State

--- src/components/Shortlinks/EmptyState.tsx
+++ src/components/Shortlinks/EmptyState.tsx
@@ -4,19 +4,16 @@ const EmptyState = () => {
   const setIsModalVisible = useModalStore((state) => state.setIsVisible);
 
   return (
-    <>
-      <p className="text-sm text-black/70">*Crickets*</p>
-      <p className="mt-2 text-sm text-black/70">
-        No links found. Why don&apos;t you{" "}
-        <span
-          className="underline hover:no-underline hover:text-black cursor-pointer"
-          onClick={() => setIsModalVisible(true)}
-        >
-          add one
-        </span>
-        ?
-      </p>
-    </>
+    <p className="mt-2 text-sm text-black/70">
+      No links found. Why don&apos;t you{" "}
+      <span
+        className="underline hover:no-underline hover:text-black cursor-pointer"
+        onClick={() => setIsModalVisible(true)}
+      >
+        add one
+      </span>
+      ?
+    </p>
   );
 };
 
