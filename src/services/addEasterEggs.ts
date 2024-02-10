484b831a7f842e2b5f902fddac199e85bda43159
Arthur Geel <ageel@enrise.com>
2024-02-10 10:36:37 +0100

    Bugfix: Prevent Easter Egg from running multiple times

--- src/services/addEasterEggs.ts
+++ src/services/addEasterEggs.ts
@@ -1,6 +1,10 @@
-export const addEasterEggs = () => {
+const addCheekyTitle = () => {
   const RNG = Math.random();
-  if (RNG >= 0.99) {
+  if (RNG >= 0.95) {
     document.title = "What's UpTab? 🥕";
   }
+};
+
+export const addEasterEggs = () => {
+  addCheekyTitle();
 };
