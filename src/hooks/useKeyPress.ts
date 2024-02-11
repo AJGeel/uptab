271d5df9cc66870c2d423166e8cb70a1701914ba
Arthur Geel <ageel@enrise.com>
2024-02-11 11:14:30 +0100

    Add debugger to useKeyPress

--- src/hooks/useKeyPress.ts
+++ src/hooks/useKeyPress.ts
@@ -1,8 +1,15 @@
 import { useCallback, useEffect } from "react";
 
+const debugMode = false;
+/* eslint-disable no-console */
+
 const useKeyPress = (targetKey: string, callback: () => void) => {
   const handleKeyPress = useCallback(
     (event: KeyboardEvent) => {
+      if (debugMode) {
+        console.info(event);
+      }
+
       if (event.key === targetKey) {
         callback();
       }
