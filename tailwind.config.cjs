29960fd617a6de4a6dc504ac7be2abd20bd9d0cc
Arthur Geel <geelaj01@gmail.com>
2026-09-01 21:07:12 +0200

    Fix font name

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["DM Sans", "sans-serif"],
+        sans: ["Figtree", "sans-serif"],
       },
       colors: {
         sky: {
